e9e0ae4d8ee33096890f788ed92b826dfad6f465
Scott Halgrim <shalgrim@gmail.com>
2020-05-24 12:46:01 -0700

    day 1 part 2 in javascript

--- node/day01_2.js
+++ node/day01_2.js
@@ -3,5 +3,18 @@
 var fs = require("fs");
 
 const file_string = fs.readFileSync('../data/input01.txt').toString();
+const string_array = file_string.split("");
 
 let floor = 0;
+let i = 0;
+
+while (floor >= 0) {
+    if (string_array[i] === '(') {
+        floor += 1;
+    } else {
+        floor -= 1;
+    }
+    i += 1;
+}
+
+console.log(i);
