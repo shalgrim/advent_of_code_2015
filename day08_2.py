2f56855bcb744fdf358d69fa54faede39768191b
Scott Halgrim <shalgrim@gmail.com>
2020-05-23 20:04:52 -0700

    skeletons for day 8 part 2

---
+++ day08_2.py
@@ -0,0 +1,6 @@
+def get_encoded_len(s):
+    raise NotImplementedError
+
+
+def get_encoded_file_diff(fn):
+    raise NotImplementedError
--- test_day08.py
+++ test_day08.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from day08_1 import get_file_diffs, get_string_length
+from day08_2 import get_encoded_len, get_encoded_file_diff
 
 
 class TestDay08(TestCase):
@@ -11,3 +12,9 @@ class TestDay08(TestCase):
         self.assertEqual(get_string_length(r'"abc"'), 3)
         self.assertEqual(get_string_length(r'"aaa\"aaa"'), 7)
         self.assertEqual(get_string_length(r'"\x27"'), 1)
+
+    def test_get_encoded_len(self):
+        self.assertTrue(False)
+
+    def test_get_encoded_file_diff(self):
+        self.assertTrue(False)
